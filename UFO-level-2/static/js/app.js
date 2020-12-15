c50ec6141f21fb20cea77bc29ef9247a8c0efa17
carolyna131 <carolyna131@gmail.com>
2020-12-14 22:36:08 -0500

    updated code

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,113 +2,51 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-console.log(tableData)
-
-// create reference to table body
 var tbody = d3.select("tbody");
 
 
-// Loop through data array
-tableData.forEach(ufoSightings => {
-
-    // append one table row for each ufo object
+data.forEach(function (ufoSightings) {
     var row = tbody.append("tr");
-
-    // use object.entries and forEach to iterate through keys and values (day 2 activity 7 & 8 and Day 3 act 3) entries gets you both the Key & the value
-    Object.entries(ufoSightings).forEach(([key, value]) => {
-        // append one cell per  value within data set (table headers in html)
+    Object.entries(ufoSightings).forEach(function ([key, value]) {
+        console.log(key, value);
         var cell = row.append("td");
         cell.text(value);
     });
 });
 
-// select the button
+// Select the filter button
 var filterButton = d3.select("#filter-btn");
-console.log("Selecting Button")
-console.log(filterButton);
 
-// select form (from office hours with Mo)
-var form = d3.select("#datetime");
-console.log("Selecting Form")
-console.log(form);
+filterButton.on("click", function () {
+  
+    var date = document.getElementById('datetime').value;
+    var city = document.getElementById('city').value;
+    var state = document.getElementById('state').value;
+    var country = document.getElementById('country').value;
+    var shape = document.getElementById('shape').value;
+    console.log(date);
+    console.log(city);
+    console.log(state);
+    console.log(country);
+    console.log(shape);
 
-// Create event handlers
-filterButton.on("click", runEnter);
-form.on("submit", runEnter);
+//use input on form to filter data by datetime in data.js file
+var userInput = tableData.filter(item => item.datetime.includes(date) && item.city.includes(city) && item.state.includes(state) && item.country.includes(country) && item.shape.includes(shape));
+    console.log(userInput);
+  
 
-// Complete the event handler function for the form
-function runEnter() {
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-
-    // clear the table
-    tbody.html("");
-
-    // Select the input element and get the raw HTML node (need 6 variables to cover all sections in html)
-    var inputElementDate = d3.select("#datetime");
-    var inputElementCity = d3.select("#city");
-    var inputElementState = d3.select("#state");
-    var inputElementCountry = d3.select("#country");
-    var inputElementShape = d3.select("#shape");
-
-
-    // Get the value property of the input element 6 variables
-    var inputValueDate = inputElementDate.property("value");
-    var inputValueCity = inputElementCity.property("value");
-    var inputValueState = inputElementState.property('value');
-    var inputValueCountry = inputElementCountry.property('value');
-    var inputValueShape = inputElementShape.property('value');
-
-
-    console.log(inputValueDate);
-    console.log(inputValueCity);
-    console.log(inputValueState);
-    console.log(inputValueCountry);
-    console.log(inputValueShape);
-    
-
-    // use input on form to filter data by datetime in data.js file per Dan, can chain the filters to get data needed. will need an or statement so no matter how many filters entered it will run.
-    // from office hours 12.12 - use || ! so that if the filter does not have data it skips that filter and returns only the filters that have input then Mo helped after class
-    // https://forum.freecodecamp.org/t/filter-multiple-condition-javascript/341164/3 match the above variables
-    var filteredData = tableData.filter((ufoSightings) => ufoSightings.datetime === inputValueDate || !inputValueDate)
-        .filter(ufoSightings => ufoSightings.city === inputValueCity || !inputValueCity)
-        .filter(ufoSightings => ufoSightings.state === inputValueState || !inputValueState)
-        .filter(ufoSightings => ufoSightings.country === inputValueCountry || !inputValueCountry)
-        .filter(ufoSightings => ufoSightings.shape === inputValueShape || !inputValueShape);
-        
-
-    console.log(filteredData);
-
-    // Use filtered data to repopulate the table
-    filteredData.forEach(function (userInput) {
-        var row = tbody.append("tr");
-        Object.entries(userInput).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
+  
+//filtered data is used to populate the tabel   
+filteredData.forEach(function(userInput) {
+  var row = tbody.append("tr");
+  Object.entries(userInput).forEach(function([key, value]) {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
 };
-
-// from afterhours with Mo and Dez
-// create showData function which resets the table and then pass that function on the reset filter button
-function showData() {
-    var tableData = data;
-    tableData.forEach(ufoSightings => {
-
-        // append one table row for each ufo object
-        var row = tbody.append("tr");
-    
-        // use object.entries and forEach to iterate through keys and values (day 2 activity 7 & 8 and Day 3 act 3) entries gets you both the Key & the value
-        Object.entries(ufoSightings).forEach(([key, value]) => {
-            // append one cell per  value within data set (table headers in html)
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-};
-
 
 // resets form
 buttonClear.on("click", function () {
-    showData(tableData)
+showData(tableData)
 });
